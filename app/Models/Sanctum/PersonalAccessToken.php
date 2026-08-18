640a625acceeeed226cc313bb93d3472045c1648
phucloc217 <loc.nguyen456789@gmail.com>
2026-08-18 20:40:57 +0700

    modified:   app/Models/Sanctum/PersonalAccessToken.php

--- app/Models/Sanctum/PersonalAccessToken.php
+++ app/Models/Sanctum/PersonalAccessToken.php
@@ -1,5 +1,5 @@
 <?php
-namespace App\Models;
+namespace App\Models\Sanctum;
 use Laravel\Sanctum\PersonalAccessToken as SanctumPersonalAccessToken;
 
 class PersonalAccessToken extends SanctumPersonalAccessToken
